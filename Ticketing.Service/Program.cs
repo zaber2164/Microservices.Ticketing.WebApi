c1dd8d9c91a5f2ed8892f40db9fff719d6191508
Abdullah Al Ameen Zaber <zaber.bd@asa-international.com>
2024-04-04 14:28:46 +0600

    masstransit added
    masstransit added & configured. need to check the configuration more.

--- Ticketing.Service/Program.cs
+++ Ticketing.Service/Program.cs
@@ -1,7 +1,22 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//MassTransit
+builder.Services.AddMassTransit(x =>
+{
+    x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(config =>
+    {
+        //config.UseHealthCheck(provider);
+        config.Host(new Uri("rabbitmq://localhost"), h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    }));
+});
+builder.Services.AddMassTransitHostedService();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
